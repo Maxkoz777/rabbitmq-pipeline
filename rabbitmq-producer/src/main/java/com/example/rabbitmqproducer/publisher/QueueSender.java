2f5d2661f3dbf95c2552e220c2ae16d5e98b418a
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-11-23 15:48:00 +0300

    RabbitMQ producer works

---
+++ rabbitmq-producer/src/main/java/com/example/rabbitmqproducer/publisher/QueueSender.java
@@ -0,0 +1,20 @@
+package com.example.rabbitmqproducer.publisher;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class QueueSender {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    private final Queue queue;
+
+    public void send(String order) {
+        rabbitTemplate.convertAndSend(this.queue.getName(), order);
+    }
+
+}
