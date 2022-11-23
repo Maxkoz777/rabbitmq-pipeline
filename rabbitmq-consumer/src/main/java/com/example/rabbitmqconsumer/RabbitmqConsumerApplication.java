f7ff4ffa664f8b939aa334574fd646db650ac538
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-11-23 15:33:07 +0300

    Consumer configured successfully

--- rabbitmq-consumer/src/main/java/com/example/rabbitmqconsumer/RabbitmqConsumerApplication.java
+++ rabbitmq-consumer/src/main/java/com/example/rabbitmqconsumer/RabbitmqConsumerApplication.java
@@ -1,9 +1,12 @@
 package com.example.rabbitmqconsumer;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@EnableRabbit
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class RabbitmqConsumerApplication {
 
     public static void main(String[] args) {
---
+++ rabbitmq-consumer/src/main/java/com/example/rabbitmqconsumer/consumer/QueueConsumer.java
@@ -0,0 +1,15 @@
+package com.example.rabbitmqconsumer.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class QueueConsumer {
+    @RabbitListener(queues = {"${queue.name}"})
+    public void receive(@Payload String fileBody) {
+        log.info("Message {}", fileBody);
+    }
+}
