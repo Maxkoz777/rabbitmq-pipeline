92fac65f2d22337d961e76060817d0874452df50
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-11-26 15:13:32 +0300

    Consumer refactored to make use of auto-configurable datasource

--- rabbitmq-consumer/src/main/java/com/example/rabbitmqconsumer/RabbitmqConsumerApplication.java
+++ rabbitmq-consumer/src/main/java/com/example/rabbitmqconsumer/RabbitmqConsumerApplication.java
@@ -3,10 +3,9 @@ package com.example.rabbitmqconsumer;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @EnableRabbit
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class RabbitmqConsumerApplication {
 
     public static void main(String[] args) {
