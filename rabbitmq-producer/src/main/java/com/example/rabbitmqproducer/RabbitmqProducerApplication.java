8ff98efde494ddd196b6a1f0e3d897725a566088
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-11-26 15:14:06 +0300

    Scheduling added to context of Producer application

--- rabbitmq-producer/src/main/java/com/example/rabbitmqproducer/RabbitmqProducerApplication.java
+++ rabbitmq-producer/src/main/java/com/example/rabbitmqproducer/RabbitmqProducerApplication.java
@@ -2,12 +2,15 @@ package com.example.rabbitmqproducer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class RabbitmqProducerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RabbitmqProducerApplication.class, args);
+        // docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.9-management
     }
 
 }
